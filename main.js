4460e3b63cec8ea5a7617916da1e6d66156e4e57
Ali <forever.alinur@gmail.com>
2021-11-18 01:05:45 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -12,6 +12,12 @@
     const field = document.createElement('fieldset');
     const lbl = document.createElement('label');
     const inputName = document.createElement('input');
+    const validEr1 = document.createElement('div');
+    const validEr2 = document.createElement('div');
+    const validEr3 = document.createElement('div');
+    const validEr4 = document.createElement('div');
+    const validEr5 = document.createElement('div');
+    const validEr6 = document.createElement('div');
     const inputSurname = document.createElement('input');
     const inputMidname = document.createElement('input');
     const buttonWrapper = document.createElement('div');
@@ -22,7 +28,7 @@
     const birthDate = document.createElement('input');
     const lblStudyDate = document.createElement('label');
     const studyDate = document.createElement('input');
-    lblFaculty = document.createElement('label')
+    const lblFaculty = document.createElement('label')
     const faculty = document.createElement('input');
     
     form.classList.add('form','mb-3', 'row');
@@ -33,6 +39,18 @@
     inputName.classList.add('form-control', 'mb-3', 'col');
     inputName.id = 'validationServer01';
     inputName.placeholder = 'Введите Имя';
+    validEr1.classList.add('error');
+    validEr1.textContent = '*Обязательно заполните поле Имя';
+    validEr2.classList.add('error');
+    validEr2.textContent = '*Обязательно заполните поле Фамилия';
+    validEr3.classList.add('error');
+    validEr3.textContent = '*Обязательно заполните поле Отчество';
+    validEr4.classList.add('error');
+    validEr4.textContent = '*Обязательно введите дату рождения';
+    validEr5.classList.add('error');
+    validEr5.textContent = '*Обязательно введите год начала обучения';
+    validEr6.classList.add('error');
+    validEr6.textContent = '*Обязательно заполните поле Факультет';
     inputSurname.classList.add('form-control', 'mb-3');
     inputSurname.placeholder = 'Введите Фамилию';
     inputMidname.classList.add('form-control');
@@ -62,8 +80,8 @@
     faculty.id = 'faculty'
     
     buttonWrapper.append(button);
-    field.prepend(lbl, inputName, inputSurname, inputMidname, );
-    fieldDate.prepend(lblbirth, birthDate, lblStudyDate, studyDate, lblFaculty, faculty);
+    field.prepend(lbl, inputName, validEr1, inputSurname, validEr2, inputMidname, validEr3);
+    fieldDate.prepend(lblbirth, birthDate, validEr4, lblStudyDate, studyDate, validEr5, lblFaculty, faculty, validEr6,);
     form.prepend(field, fieldDate, buttonWrapper );
     
     return {
@@ -74,7 +92,13 @@
       birthDate,
       studyDate,
       faculty,
-      form
+      form,
+      // validEr1,
+      // validEr2,
+      // validEr3,
+      // validEr4,
+      // validEr5,
+      // validEr6,
     }
   };
   
@@ -121,7 +145,7 @@
       studytd.textContent = studytdtxt; 
       
       str.prepend(sth, fullNametd, facultytd, birthtd, studytd);
-      tbody.prepend(str)
+      tbody.append(str)
     };
 
     return {
@@ -137,21 +161,71 @@
     const title = createTitle('Students');
     const form = createForm()
     const table = createTable();
+    let count = 0;
     
+
     container.append(title);
     container.appendChild(form.form);
     container.appendChild(table.table);
     form.form.addEventListener('submit', (e) => {
       e.preventDefault();
       
+
       const createFullName = `${form.inputName.value} ${form.inputSurname.value} ${form.inputMidname.value}`;
       
-      if(createFullName.trim().length == 6) {
-        return
-          };
+      if(createFullName.trim().length >= 11 && form.faculty.value.trim().length > 2 && form.birthDate.value.length > 0 && form.studyDate.value.length > 0) {
+        count++
+        // birthDate
+        let dateAge = form.birthDate.value;
+        dateStart = Date.parse(dateAge);
+        let now = new Date();
+        now = Date.parse(now);
+        let age = now - dateStart;
+        age = new Date(age).toISOString().substr(0, 4);
+        age = Number(age) - 1970;
+        dateAge = String(dateAge).split('-').reverse().join('.')
+        dateAge = `${dateAge} (${age} лет)`
+        // studyDate
+        let startStudy = form.studyDate.value;
+        startStudyS = Date.parse(startStudy);
+        let course = now - startStudyS;
+        course = new Date(course).toISOString().substr(0, 4);
+        course = (Number(course) - 1970) + 1;
+        function checkCourse() {
+          if(course > 4) {
+            course = 'Закончил';
+          } else {
+            course += ' курс'
+          }
+        };
+          checkCourse();
+        startStudy = String(startStudy).split('-').join('.').substr(0, 4);
+        const endStudy = Number(startStudy) + 4;
+        const yearStudy = `${startStudy}-${endStudy}`;
+        const dateStudy = `${yearStudy} (${course})`
+         
+        const column = table.createColumn( count, createFullName, form.faculty.value , dateAge, dateStudy);
+        
+        const input = Array.from(document.querySelectorAll('input'));
+        input.map((el) => {
+          el.classList.add('mb-3')
+        });
 
-      const column = table.createColumn( '1', createFullName, form.faculty.value, form.birthDate.value, form.studyDate.value);
-      
+        const error = Array.from(document.querySelectorAll('.error'));
+        error.map((el) => {
+          el.classList.remove('error--active');
+        });
+      } else {
+        const input = Array.from(document.querySelectorAll('input'));
+        input.map((el) => {
+          el.classList.remove('mb-3')
+        });
+        const error = Array.from(document.querySelectorAll('.error'));
+        error.map((el) => {
+          el.classList.add('error--active');
+        });
+      }
+        
 
     })
   })
